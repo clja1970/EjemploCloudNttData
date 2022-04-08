f202c093ab96e58e8cdde0ec8f276130f026492e
Anka Motz <73638312+clja1970@users.noreply.github.com>
2022-04-08 11:13:48 +0200

    Add files via upload

---
+++ SpringCloudEjemploUno-Server/src/test/java/com/nttdata/ejemplospringcloudbootcamp/SpringCloudEjemploUno/SpringCloudEjemploUnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.ejemplospringcloudbootcamp.SpringCloudEjemploUno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEjemploUnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
